45c20ccecce944d2f96ae8ef746d62dadd5d63f9
Humpo <acomerford@albany.edu>
2016-06-11 20:21:14 -0400

    Added more infstructure 'Row' class, 'remove_space' method. In addition adjusted evaluation 'scoring' by calculating the number of completed rows when a 'Board' object is being created. This is so when the algorithm 'simulates' a new board with a piece added to it, it will not take into account fake spaces that will dissapear when complete rows will dissapear

--- metric_tetris.py
+++ metric_tetris.py
@@ -40,16 +40,14 @@ if __name__ == "__main__":
 	print "Creating Initial Batch"
 	for index in range(batch_size):#create initial job list from batch size
 		job = mult.Process(target=tetris_player_process, args=(lifetimes, jobs-1))
-		print "HELLO"
 		job.start()
 		jobs_list.append(job)
 		jobs -= 1	
 
 	while not all_jobs_finished(jobs_list) or jobs > 0:#while all the jobs are still alive and there are still more jobs
 		jobs_alive = [job.is_alive() for job in jobs_list]#get the jobs life status
-		print jobs_alive
 		if False in jobs_alive and jobs > 0:#if a job is finished and there are still jobs
-			print "adding new job"
+			print "Job finished, %d jobs left" % (jobs)
 			finished_job_index = jobs_alive.index(False)
 			jobs_list[finished_job_index].join()#find the job and join it
 			jobs_list[finished_job_index] = mult.Process(target=tetris_player_process, args=(lifetimes, jobs-1))
--- tetris_player.py
+++ tetris_player.py
@@ -31,7 +31,7 @@ rotations_by_index = {#number of rotations manually by piece location above
 
 def remove_row(board, row):
         del board[row]
-        return [[0 for i in xrange(cols)]] + board
+        return [[0 for i in xrange(10)]] + board
 
 def rotate_clockwise(shape):
         return [ [ shape[y][x]
@@ -56,6 +56,10 @@ def join_matrixes(mat1, mat2, mat2_off):
                         mat1[cy+off_y-1 ][cx+off_x] += val
         return mat1
 
+class Row(list):
+	def __init__(self, row):
+		list.__init__(self, row)
+		self.is_full = all(row)
 
 class Column(list):
 	def __init__(self, col_list):
@@ -66,6 +70,10 @@ class Column(list):
 
 	def __eq__(self, other_col_list):
 		return self == other_col_list
+
+	def remove_space(self, index):
+		del self[index]
+		return [0] + self
 
 	def calc_data(self, update=False):#calculates or updates data
 		if not self.height_gap or update:
@@ -86,19 +94,28 @@ class Board(list):
 	def __init__(self, board_list):
 		list.__init__(self, [Column(col).calc_data() for col in board_list])
 		self.max = False
+		self.min = False
 		self.average = False
 		self.mode = False
 		self.total_spaces = False
 		self.full_rows = False
+
+	def invert(self):
+		return [list(row) for row in zip(*self)]
 	
-	def calc_data(self, update=False):
+	def calc_data(self, update=False):		
 		if not self.full_rows or update:
-			if len(zip(*self)[0]) == 10:
-				self.full_rows = sum([1 if all(row) else 0 for row in zip(*self)])
-			else:
-				self.full_rows = 1
+			self.full_rows = 1
+			row_board = self.invert()							#get the board in row format
+			if row_board[0] == 10:								#when the board isn't a slice
+				for index, row in enumerate(row_board[:-1]):	#loop through each row
+					if all(row):								#if the row is complete
+						for col in self:col.remove_space(index)	#delete that space in each column
+						self.full_rows += 1 					#increment the number of rows
 		if not self.max or update:
 			self.max = max(col.height for col in self)
+		if not self.min or update:
+			self.min = min(col.height for col in self)
 		if not self.average or update:
 			self.average = sum(col.height for col in self)/len(self)
 		if not self.mode or update:
@@ -125,98 +142,102 @@ class Board(list):
 			yield (col, Board(self[col:col+width][:]))
 
 	def fake_add(self, x, piece):#takes a piece at an x coordinate and returns a board object with that piece "insta dropped"
-		row_board = [list(row) for row in zip(*self)]#make the cols into rows again
-		
-		for y in range(1, len(row_board)):
-			if check_collision(row_board, piece, (x, y)):
-				return Board(zip(*join_matrixes(row_board, piece, (x, y))))
+		row_board = self.invert()													#get the board in col format
+		for y in range(1, len(row_board)):											#loop through index's of row_board
+			if check_collision(row_board, piece, (x, y)):							#if there is a collision at "y"
+				row_board_with_piece = join_matrixes(row_board, piece, (x, y))		#add the piece to the board
+				return Board(zip(*row_board_with_piece))							#return new Board in original format
 
 	def score(self):
-		return float(self.full_rows) / (self.max + self.average + self.mode + self.total_spaces + 1)
+		return float(self.full_rows) / (self.max + self.min + self.average + self.mode + self.total_spaces + 1)
 
 class player_process(Thread):
-    def __init__(self, app):
-        Thread.__init__(self)
-	self.exit = Event()
-	self.app = app
+	def __init__(self, app):
+		Thread.__init__(self)
+		self.exit = Event()
+		self.app = app
 
-    def get_rotations(self, shape):#iter through each rotoation of a shape
-	shape = shape
-	for r in range(3):
-	 	try:
-			rotations = rotations_by_index[tetris_shapes.index(shape)]#gets the number of rotations manually
-		except ValueError:
-			shape = rotate_clockwise(shape)
+	def get_rotations(self, shape):#iter through each rotoation of a shape
+		shape = shape
+		for r in range(3):
+		 	try:
+				rotations = rotations_by_index[tetris_shapes.index(shape)]#gets the number of rotations manually
+			except ValueError:
+				shape = rotate_clockwise(shape)
 
-	rotated_shape = shape
-	for r in range(rotations):
-		rotated_shape = rotate_clockwise(rotated_shape)
-		yield rotated_shape
+		rotated_shape = shape
+		for r in range(rotations):
+			rotated_shape = rotate_clockwise(rotated_shape)
+			yield rotated_shape
 
-    def simple_play(self):
-	board = Board(zip(*self.app.board))
-	scores = {}
+	def simple_play(self):
+		board = Board(zip(*self.app.board))
+		scores = {}
 
-	for rotated_stone in self.get_rotations(self.app.stone):#iter through each rotation
+		for rotated_stone in self.get_rotations(self.app.stone):#iter through each rotation
 
-		for slice_index, slice in board.slice_iter(len(zip(*rotated_stone))):#iter through each slice				
+			for slice_index, slice in board.slice_iter(len(zip(*rotated_stone))):#iter through each slice				
 
-			slice_without_piece = slice.calc_data()
-			board_without_piece = board.calc_data()	
-			#renaming for ease of use
+				slice_without_piece = slice.calc_data()
+				board_without_piece = board.calc_data()	
+				#renaming for ease of use
 
-			slice_with_piece = slice.fake_add(0, rotated_stone).calc_data()
-			board_with_piece = board.fake_add(slice_index, rotated_stone).calc_data()			
+				slice_with_piece = slice.fake_add(0, rotated_stone).calc_data()
+				board_with_piece = board.fake_add(slice_index, rotated_stone).calc_data()			
 
-			#without_score = slice_without_piece.score() + board_without_piece.score()
-			with_score = slice_with_piece.score() + board_with_piece.score()
-			total_score = with_score
-	
-			scores[total_score] = (slice_index, rotated_stone)
+				#without_score = slice_without_piece.score() + board_without_piece.score()
+				with_score = slice_with_piece.score() + board_with_piece.score()
+				total_score = with_score
+
+				#for i in board_with_piece:print i
+				#board_with_piece.data()
+				#print total_score
+				#print (slice_index, rotated_stone)
 		
-	best_move = scores[max(scores.keys())]
-		
-	while self.app.stone != best_move[1] and not self.app.gameover:
-		self.app.rotate_stone()
-		
-	move_int = best_move[0] - self.app.stone_x
-	self.app.move(move_int)
-		
-	self.app.insta_drop()
+				scores[total_score] = (slice_index, rotated_stone)
+			
+		best_move = scores[max(scores.keys())]
+			
+		while self.app.stone != best_move[1] and not self.app.gameover:
+			self.app.rotate_stone()
+			
+		move_int = best_move[0] - self.app.stone_x
+		self.app.move(move_int)
+			
+		self.app.insta_drop()
 
-	sleep(0.05)
+		sleep(0.1)
 
-    def run(self):
-	debug = True
-	player_pieces_processed = 0
+	def run(self):
+		debug = True
+		player_pieces_processed = 0
 
-	while not self.exit.is_set() and self.app.auto.wait():
-		
-		#this statement is to make sure each piece is only processed once
-		#compares the number of pieces processed by the game and by the player
-		if self.app.pieces_processed != player_pieces_processed:
+		while not self.exit.is_set() and self.app.auto.wait():
+			
+			#this statement is to make sure each piece is only processed once
+			#compares the number of pieces processed by the game and by the player
+			if self.app.pieces_processed != player_pieces_processed:
+				player_pieces_processed += 1
+				continue
+			
+			self.simple_play()
+			
 			player_pieces_processed += 1
-			continue
-		
-		self.simple_play()
-		
-		player_pieces_processed += 1
-		#sleep(0.5)
-	print "process has ended"
+			#sleep(0.5)
+		print "process has ended"
 
-    def debug(self):
-	print "======DEBUG MODE======"
-	
-	print "BOARD"
-	for i in self.app.board:print i		
+	def debug(self):
+		print "======DEBUG MODE======"
+		print "BOARD"
+		for i in self.app.board:print i		
 
-	print "CURRENT PIECE"
-	for i in self.app.stone:print i
+		print "CURRENT PIECE"
+		for i in self.app.stone:print i
 
-	#print "NEXT PIECE"
-	#for i in self.app.next_stone:print i
+		#print "NEXT PIECE"
+		#for i in self.app.next_stone:print i
 
-    def shutdown(self):
-	print "executing shutdown"
-        self.exit.set()
-	self.app.auto.set()
+	def shutdown(self):
+		print "executing shutdown"
+		self.exit.set()
+		self.app.auto.set()
